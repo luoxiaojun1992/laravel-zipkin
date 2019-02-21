0edb8aaf513967ca5a06945ed84670e9dde44097
luoxiaojun <luoxiaojun@dodoca.com>
2019-02-21 17:36:49 +0800

    add provider

---
+++ src/Provider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lxj\Laravel\Zipkin;
+
+use Illuminate\Support\ServiceProvider;
+
+class Provider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/config/zipkin.php' => config_path('zipkin.php')], 'config');
+    }
+}
