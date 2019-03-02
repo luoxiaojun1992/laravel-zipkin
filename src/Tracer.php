fd179af31b0b693f4ec89c1aa7a5abaf5015c228
luoxiaojun <luoxiaojun@dodoca.com>
2019-03-02 13:31:36 +0800

    fix span name policy

--- src/Tracer.php
+++ src/Tracer.php
@@ -264,6 +264,20 @@ class Tracer
     }
 
     /**
+     * Formatting http path
+     *
+     * @param $httpPath
+     * @return string|string[]|null
+     */
+    public function formatHttpPath($httpPath)
+    {
+        $httpPath = preg_replace('/\/\d+$/', '/{id}', $httpPath);
+        $httpPath = preg_replace('/\/\d+\//', '/{id}/', $httpPath);
+
+        return $httpPath;
+    }
+
+    /**
      * Add span tag
      *
      * @param Span $span
