6bee3b95a35df3d0e06f5b1b26c4b891ec8acae4
luoxiaojun <luoxiaojun@dodoca.com>
2020-05-28 12:14:42 +0800

    fix

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -25,10 +25,18 @@ class HttpClient extends GuzzleHttpClient
      * @param string $spanName
      * @param bool $injectSpanCtx
      * @param bool $traceInConsole
+     * @param bool $flushTracing
      * @return mixed|\Psr\Http\Message\ResponseInterface|null
      * @throws \Exception
      */
-    public function send(RequestInterface $request, array $options = [], $spanName = null, $injectSpanCtx = true, $traceInConsole = false)
+    public function send(
+        RequestInterface $request,
+        array $options = [],
+        $spanName = null,
+        $injectSpanCtx = true,
+        $traceInConsole = false,
+        $flushTracing = false
+    )
     {
         $sendRequest = function () use ($request, $options) {
             try {
@@ -97,6 +105,6 @@ class HttpClient extends GuzzleHttpClient
                         }
                     }
                 }
-            });
+            }, $flushTracing);
     }
 }
